9f8b39c8ea14c9dfeb091b6f84be7bd4465ad7cb
jakekirsch2 <jakekirsch11@gmail.com>
2023-05-31 15:40:47 -0500

    cloudbuild is owner

--- projects/main.tf
+++ projects/main.tf
@@ -33,39 +33,39 @@ resource "google_project_iam_binding" "kirsch_becker" {
   ]
 }
 
-# resource "google_storage_bucket" "kirsch_becker_data" {
-#   name          = "kirsch-becker-data"
-#   project       = "kirsch-becker"
-#   location      = "us-central1"
-#   force_destroy = true
-#   depends_on    = [google_project_iam_binding.kirsch_becker]
-# }
+resource "google_storage_bucket" "kirsch_becker_data" {
+  name          = "kirsch-becker-data"
+  project       = "kirsch-becker"
+  location      = "us-central1"
+  force_destroy = true
+  depends_on    = [google_project_iam_binding.kirsch_becker]
+}
 
-# resource "google_project_iam_member" "composer-service-agent" {
-#   project = "kirsch-becker"
-#   role    = "roles/composer.ServiceAgentV2Ext"
-#   member  = "serviceAccount:service-533271204219@cloudcomposer-accounts.iam.gserviceaccount.com"
-#   depends_on = [google_project_service.services["composer.googleapis.com"]]
-# }
+resource "google_project_iam_member" "composer-service-agent" {
+  project = "kirsch-becker"
+  role    = "roles/composer.ServiceAgentV2Ext"
+  member  = "serviceAccount:service-533271204219@cloudcomposer-accounts.iam.gserviceaccount.com"
+  depends_on = [google_project_service.services["composer.googleapis.com"]]
+}
 
-# resource "google_composer_environment" "composer" {
-#   project = "kirsch-becker"
-#   name    = "kirsch-data-platform-composer-environment"
-#   region  = "us-central1"
-#   config {
-#     software_config {
-#       image_version = "composer-2-airflow-2"
-#     }
-#   }
+resource "google_composer_environment" "composer" {
+  project = "kirsch-becker"
+  name    = "kirsch-data-platform-composer-environment"
+  region  = "us-central1"
+  config {
+    software_config {
+      image_version = "composer-2-airflow-2"
+    }
+  }
 
-#   depends_on = [google_project_service.services["composer.googleapis.com"], google_project_iam_member.composer-service-agent]
-# }
+  depends_on = [google_project_service.services["composer.googleapis.com"], google_project_iam_member.composer-service-agent]
+}
 
 
-# resource "google_artifact_registry_repository" "repositories" {
-#   project       = "kirsch-becker"
-#   location      = "us-central1"
-#   repository_id = "docker-repository"
-#   format        = "DOCKER"
-#   depends_on    = [google_project_service.services["artifactregistry.googleapis.com"]]
-# }
+resource "google_artifact_registry_repository" "repositories" {
+  project       = "kirsch-becker"
+  location      = "us-central1"
+  repository_id = "docker-repository"
+  format        = "DOCKER"
+  depends_on    = [google_project_service.services["artifactregistry.googleapis.com"]]
+}
